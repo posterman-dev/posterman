79913b98c7c6f37febc35fa3dfb35e7b16d67b5f
Nishi <nishi@nishi.boats>
2025-03-22 18:09:36 +0900

    static

--- mk/p_generic.mk
+++ mk/p_generic.mk
@@ -1,5 +1,8 @@
 CC = cc -I. -c -o
+AR = ar
 LINK = cc -o
 PERL = perl
 OBJ = .o
 EXEC =
+STLIB = .a
+DYLIB = .so
--- mk/p_win64.mk
+++ mk/p_win64.mk
@@ -1,5 +1,8 @@
 CC = x86_64-w64-mingw32-gcc -I. -c -o
+AR = x86_64-w64-mingw32-ar
 LINK = x86_64-w64-mingw32-gcc -o
 PERL = perl
 OBJ = .o
 EXEC = .exe
+STLIB = .a
+DYLIB = .dll
